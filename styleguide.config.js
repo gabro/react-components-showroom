48050e6e24cc13b5aacf9ad3f132d7c12330f382
Gabriele Petronella <gabriele@buildo.io>
2017-08-22 14:11:05 +0200

    Add Badge

--- styleguide.config.js
+++ styleguide.config.js
@@ -29,7 +29,8 @@ module.exports = {
     {
       name: 'Components',
       components: () => [
-        brc('AsyncStatusIndicator')
+        brc('AsyncStatusIndicator'),
+        brc('Badge')
       ],
     }
   ],
